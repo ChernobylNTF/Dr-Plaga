aea7d29130cbba8aaec6d57dbc10d79a40a0cd9c
ChernobylNTF <wallydd1983@gmail.com>
2025-04-25 13:34:12 +0000

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import GameContainer from "@/components/game-container"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-gray-900 text-white">
+      <GameContainer />
+    </main>
+  )
+}
